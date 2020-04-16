1efd3c2181a6bbf69bcb03ffe304500f46b589d3
Callum France RPi <france.callum@gmail.com>
2020-04-16 10:45:44 +0100

    Pi gpiozero test works with IO

--- pi/test_gpio_in_out.py
+++ pi/test_gpio_in_out.py
@@ -2,15 +2,18 @@ import gpiozero
 
 pinout = [18, 23, 24, 25, 12, 16]
 pinin = [2, 3, 4, 17, 27, 22]
-p_out = [ gpiozero.LED(p) for p in pinout ]
-p_in = [ gpiozero.Button(p) for p in pinin ]
 
-for i in p_in:
-    i.bounce_time = 0.25
+p_out = [ gpiozero.LED(p) for p in pinout ]
+
+p_in = [ gpiozero.Button(p, bounce_time=0.25) for p in pinin ]
+
+for i in p_out:
+        i.on()
+
+for n, i in enumerate(p_in):
+	p_in[n].when_pressed = p_out[n].toggle 
+
+print("Setup completed\n")
 
 while True:
-    for i in p_out:
-        i.off()
-
-    for n, i in enumerate(p_in):
-       p_in[n].when_pressed = p_out[n].toggle 
+	pass
